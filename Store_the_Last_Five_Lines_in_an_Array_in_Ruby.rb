162b11cc36e6b18e5630679a927878aa13125fa3
toddt67878 <48630393+toddt67878@users.noreply.github.com>
2019-08-28 17:21:02 -0500

    Add files via upload

---
+++ Store_the_Last_Five_Lines_in_an_Array_in_Ruby.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+
+def file_tail path, num
+  %x{tail -#{num} #{path}}.split("\n")
+end
+
+describe 'file_tail' do
+  it 'can retrieve the last 5 lines from a file and store it in an array' do
+    expect(file_tail('support/emails.txt', 5)).to eq([
+      "Satisfied with Smile Dental? Review us on Yelp!",
+      "How was your service at ZenSpa?",
+      "Thanks for visiting! Here’s a link to submit your review",
+      "Please share your experience with us!",
+      "Let’s be friends—Like and Review us on Facebook!"
+    ])
+  end
+end
